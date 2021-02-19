bb370fe175051635e4309465b77ba78a36de6c37
nstppbl <1nstppbl@gmail.com>
2021-02-19 23:09:03 +0500

    init: make function for reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString()
+    let arr = str.split("")
+    let revArr = []
+    let reversedString = 0
+    let resNumber = 0
+    
+for (let i = arr.length - 1; i>=0; i--) {
+     revArr.push(arr[i]) 
 }
+    reversedString = revArr.join("")
+    return parseInt(reversedString)
+
+}
+
